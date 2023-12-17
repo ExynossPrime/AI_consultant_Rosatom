712e91aaaa858c4d5f06d7a79bff245cd03d648d
Danila Toropov <toropov-danila2004@mail.ru>
2023-12-17 04:46:33 +0300

    model 2

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,7 @@ from telebot import types
 
 # Авторизация в сервисе GigaChat
 chat = GigaChat(credentials='ZWNhNjg5MDEtOGIxYS00Nzg1LWFiOTAtMjQ4ZjhmYjViZGIzOjZkNjk3ZWEyLTkwYTgtNDNmNC04YTIzLTMwODljY2QyZDA2OQ==', verify_ssl_certs=False)
-bot = telebot.TeleBot('6837296858:AAHJtUPKsPSIUjDyXFrydQkKIslBROq1Fx0')
+bot = telebot.TeleBot('6837296858:AAFSVSodG1VsO46BCA3x4fcQLBj1YUl8Qas')
 
 # messages = [
 #     SystemMessage(
@@ -27,9 +27,16 @@ messages = [
 
 # messages = [
 #     SystemMessage(
+#         content="Ты работаешь в отделе закупок организации и ищешь ответы в нормативно-правовых актах Российской Федерации"
+#     )
+# ]
+
+# messages = [
+#     SystemMessage(
 #         content="Ты работаешь в отделе закупок государственной организации Росатом и ищешь ответы в нормативно-правовых актах Росатома и ЕОСЗ"
 #     )
 # ]
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -48,15 +55,7 @@ def get_text_messages(message):
     messages.append(HumanMessage(content=text))
     res = chat(messages)
     messages.append(res)
-    # messages.append('и в каком нормативно-правовом акте это указано')
     print(res.content)
     bot.send_message(message.chat.id, res.content)
 
 bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
-
-# while (True):
-#     user_input = input("User: ")
-#     messages.append(HumanMessage(content=user_input))
-#     res = chat(messages)
-#     messages.append(res)
-#     print("Bot: ", res.content)
